04c409a90e16ac77b2851607d035944e0591e3ff
isuri54 <154318794+isuri54@users.noreply.github.com>
2025-12-02 23:26:52 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Signup from './components/Signup';
 import Login from './components/Login';
+import Home from './components/Home';
+import Provider from './components/Provider';
+import ProviderDashboard from './components/ProviderDashboard';
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
         <Route path="/" element={<Signup />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/provider" element={<Provider/>}/>
+        <Route path="/provider-dashboard" element={<ProviderDashboard />} />
       </Routes>
     </Router>
   );
