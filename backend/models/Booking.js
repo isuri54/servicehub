d442d5cce0e7bbd2a0e29622c907793081e86381
isuri54 <154318794+isuri54@users.noreply.github.com>
2025-12-04 00:56:08 +0530

    Update Booking.js

--- backend/models/Booking.js
+++ backend/models/Booking.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const bookingSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  providerId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  providerId: { type: mongoose.Schema.Types.ObjectId, ref: "Provider", required: true },
   dateRange: {
     start: { type: Date, required: true },
     end: { type: Date, required: true },
