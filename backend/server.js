3600586c42e085cff987a7a35eb4469bccd1017b
isuri54 <154318794+isuri54@users.noreply.github.com>
2025-12-03 11:18:15 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const path = require('path');
 const authRoutes = require('./routes/auth');
 const provider = require('./routes/provider');
 const providerRoutes = require('./routes/providerRoutes'); 
+const providerProfileView = require('./routes/providerProfileView');
 
 dotenv.config();
 const app = express();
@@ -16,6 +17,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/auth', authRoutes);
 app.use('/api/provider', provider);
 app.use('/api/providers', providerRoutes);
+app.use('/api/providerprofile', providerProfileView)
 
 
 app.get('/', (req, res) => {
