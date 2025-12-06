30fccdb752fe3104275924d6e6a75ab2b93bafe4
isuri54 <154318794+isuri54@users.noreply.github.com>
2025-12-07 01:42:49 +0530

    Update Provider.js

--- backend/models/Provider.js
+++ backend/models/Provider.js
@@ -10,7 +10,11 @@ const providerSchema = new mongoose.Schema({
   education: { type: String, required: true },
   experience: { type: String, required: true },
   rating: { type: Number, default: 0 },
-  reviewCount: { type: Number, default: 0 }
+  reviewCount: { type: Number, default: 0 },
+  workingDays: [{ type: String }],
+  startTime: { type: String, default: "09:00" },
+  endTime: { type: String, default: "17:00" },
+  unavailableDates: [{ type: String }],
 }, { timestamps: true });
 
 module.exports = mongoose.model("Provider", providerSchema);
