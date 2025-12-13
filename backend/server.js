61b6d6d63b6ce3cfec4184010c10b25b618cf1fb
isuri54 <154318794+isuri54@users.noreply.github.com>
2025-12-14 00:00:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,17 @@
-const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const cors = require('cors');
-const path = require('path');
-const authRoutes = require('./routes/auth');
-const provider = require('./routes/provider');
-const providerRoutes = require('./routes/providerRoutes'); 
-const providerProfileView = require('./routes/providerProfileView');
-const bookingRoutes = require("./routes/bookingRoutes");
+const app = require('./app');
 
 dotenv.config();
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use('/api/auth', authRoutes);
-app.use('/api/provider', provider);
-app.use('/api/providers', providerRoutes);
-app.use('/api/providerprofile', providerProfileView);
-app.use("/api/bookings", bookingRoutes);
-
-
-app.get('/', (req, res) => {
-  res.json({ message: 'Server is running' });
-});
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log('MongoDB connection error:', err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(PORT, () =>
+      console.log(`Server running on port ${PORT}`)
+    );
+  })
+  .catch(err => console.error(err));
